cc407b233e12941090ca39a11353cea4d0ce9a5b
iqbalnur21 <m.iqbalnur21.min@gmail.com>
2024-04-28 11:40:06 +0700

    change folder name

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,4 @@
-import path from 'path';
-
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
-
-const nextConfig = {
-  webpack: (config) => {
-    config.resolve.alias['@'] = path.join(__dirname, 'src');
-    return config;
-  }
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
 export default nextConfig;
